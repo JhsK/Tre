363f60b915f8d618547b283404fb4444dc536a6a
JhsK <qwe6293@nate.com>
2021-07-02 16:38:19 +0900

    [Feat] 캘린더 날짜 선택 시 모달 RangePicker와 호환 및 console.log 데이터 출력 테스트

--- front/src/components/CalendarLayout.js
+++ front/src/components/CalendarLayout.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from "react";
 import styled from "styled-components";
+import moment from "moment";
 import LoginForm from "./style/LoginForm.css";
 import {
   Calendar,
@@ -21,18 +22,42 @@ const CalendarContainer = styled.div`
 const CalendarLayout = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [dateValue, setDateValue] = useState("");
+  const [planText, setPlanText] = useState("");
+  // moment("2021-07-02")
+  const [radioValue, setRadioValue] = useState("");
+  const [datePickerValue, setDatePickerValue] = useState("");
 
   const handleCancel = useCallback(() => {
     setModalVisible(false);
-  }, []);
+    console.log(dateValue);
+  }, [dateValue]);
 
   const handleOk = useCallback(() => {
     setModalVisible(false);
-  }, []);
+    console.log(datePickerValue, planText, radioValue, dateValue);
+  }, [datePickerValue, planText, radioValue, dateValue]);
 
   const onClickedDate = useCallback((e) => {
     setModalVisible(true);
-    console.log(e._d);
+    setDateValue(moment(e._d));
+  }, []);
+
+  const onChangePlanText = useCallback((e) => {
+    e.preventDefault();
+    setPlanText(e.target.value);
+  }, []);
+
+  const onChangeRadio = useCallback((e) => {
+    e.preventDefault();
+    setRadioValue(e.target.value);
+  }, []);
+
+  const onChangeDatePicker = useCallback((e) => {
+    console.log(e[0].format("YYYY-MM-DD"));
+    setDatePickerValue({
+      firstDay: e[0].format("YYYY-MM-DD"),
+      lastDay: e[1].format("YYYY-MM-DD"),
+    });
   }, []);
 
   return (
@@ -49,25 +74,34 @@ const CalendarLayout = () => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <RangePicker bordered={false} style={{ marginBottom: "2rem" }} />
+        <RangePicker
+          bordered={false}
+          format={"YYYY/MM/DD"}
+          value={[dateValue, dateValue]}
+          style={{ marginBottom: "2rem" }}
+          // value={datePickerValue}
+          onChange={onChangeDatePicker}
+        />
         <Input
           placeholder="일정을 입력해주세요"
           style={{ marginBottom: "2.5rem" }}
+          value={planText}
+          onChange={onChangePlanText}
         />
-        <Radio.Group>
-          <Radio value={1}>
+        <Radio.Group onChange={onChangeRadio}>
+          <Radio value={"success"}>
             <Badge status="success" />
           </Radio>
-          <Radio value={2}>
+          <Radio value={"error"}>
             <Badge status="error" />
           </Radio>
-          <Radio value={3}>
+          <Radio value={"default"}>
             <Badge status="default" />
           </Radio>
-          <Radio value={4}>
+          <Radio value={"processing"}>
             <Badge status="processing" />
           </Radio>
-          <Radio value={4}>
+          <Radio value={"warning"}>
             <Badge status="warning" />
           </Radio>
         </Radio.Group>
