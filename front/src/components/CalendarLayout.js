40cbb15518d7c7c964397953aee61aa3ef74557f
JhsK <qwe6293@nate.com>
2021-07-02 11:56:42 +0900

    [Feat] modal 커스텀화

--- front/src/components/CalendarLayout.js
+++ front/src/components/CalendarLayout.js
@@ -1,7 +1,17 @@
 import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import LoginForm from "./style/LoginForm.css";
-import { Calendar, Badge, PageHeader, Modal } from "antd";
+import {
+  Calendar,
+  Badge,
+  PageHeader,
+  Modal,
+  DatePicker,
+  Input,
+  Radio,
+} from "antd";
+
+const { RangePicker } = DatePicker;
 
 const CalendarContainer = styled.div`
   width: 90%;
@@ -34,14 +44,33 @@ const CalendarLayout = () => {
         style={{ width: "95%", marginLeft: "2rem" }}
       />
       <Modal
-        title="Basic Modal"
+        title="일정 추가"
         visible={modalVisible}
         onOk={handleOk}
-        // onCancel={handleCancel}
+        onCancel={handleCancel}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <RangePicker bordered={false} style={{ marginBottom: "2rem" }} />
+        <Input
+          placeholder="일정을 입력해주세요"
+          style={{ marginBottom: "2.5rem" }}
+        />
+        <Radio.Group>
+          <Radio value={1}>
+            <Badge status="success" />
+          </Radio>
+          <Radio value={2}>
+            <Badge status="error" />
+          </Radio>
+          <Radio value={3}>
+            <Badge status="default" />
+          </Radio>
+          <Radio value={4}>
+            <Badge status="processing" />
+          </Radio>
+          <Radio value={4}>
+            <Badge status="warning" />
+          </Radio>
+        </Radio.Group>
       </Modal>
     </CalendarContainer>
   );
