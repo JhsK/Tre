e6d2c654ff9a5a2503ac41653b45f455d8af7371
JhsK <qwe6293@nate.com>
2021-07-05 10:31:00 +0900

    [Feat] redux 초기 설정

--- front/src/App.js
+++ front/src/App.js
@@ -7,14 +7,19 @@ import Schedule from "./pages/schedule";
 import Calendar from "./pages/calendar";
 import Memory from "./pages/memory";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   return (
     <>
-      <Route path="/" component={Login} exact={true} />
+      {isLoggedIn ? (
+        <Route path="/" component={Schedule} exact={true} />
+      ) : (
+        <Route path="/" component={Login} exact={true} />
+      )}
       <Route path="/join" component={Join} exact={true} />
       <Route path="/password" component={PasswordFind} exact={true} />
-      <Route path="/schedule" component={Schedule} exact={true} />
       <Route path="/calendar" component={Calendar} exact />
       <Route path="/memory" component={Memory} exact />
     </>
