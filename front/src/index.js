da1dd3b7417d1e44abce951532e5b92a253b7e47
JhsK <qwe6293@nate.com>
2021-07-11 15:14:27 +0900

    [Feat] axios, reudx-saga 설치 및 초기 설정

--- front/src/index.js
+++ front/src/index.js
@@ -4,12 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers/index";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas/index";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
