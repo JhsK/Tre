9f71126c870867e29d929664489d8706bb791ef9
JhsK <qwe6293@nate.com>
2021-07-18 15:03:54 +0900

    [Feat] 라우터 분리하기

--- back/app.js
+++ back/app.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const postRouter = require("./routes/post");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hellow express");
 });
+
+app.get("/", (req, res) => {
+  res.send("hello api");
+});
+
+app.get("/posts", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      content: "hello",
+    },
+  ]);
+});
+
+app.use("/post", postRouter);
 
 app.listen(3065, () => {
   console.log("서버 실행 중");
