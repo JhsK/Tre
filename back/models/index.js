e4b2dabddb5b9b418981e050b144ee6f650a12ea
JhsK <qwe6293@nate.com>
2021-07-22 10:29:27 +0900

    [Feat] plan model 생성

--- back/models/index.js
+++ back/models/index.js
@@ -4,6 +4,7 @@ const config = require("../config/config")[env];
 
 const User = require("./user");
 const Post = require("./post");
+const Plan = require("./plan");
 const Image = require("./image");
 
 const db = {};
@@ -17,14 +18,17 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
+db.Plan = Plan;
 db.Image = Image;
 
 User.init(sequelize);
 Post.init(sequelize);
+Plan.init(sequelize);
 Image.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
+Plan.associate(db);
 Image.associate(db);
 
 module.exports = db;
--- back/models/plan.js
+++ back/models/plan.js
@@ -1,17 +1,43 @@
-module.exports = (sequelize, DataTypes) => {
-  const Plan = sequelize.define(
-    "Plan",
-    {
-      email: {},
-      nickname: {},
-      username: {},
-      password: {},
-    },
-    {
-      charset: "utf8",
-      collate: "utf8_general_ci",
-    }
-  );
-  Plan.associate = (db) => {};
-  return Plan;
+const Sequelize = require("sequelize");
+
+module.exports = class Plan extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+        },
+        start: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        end: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        dateValue: {
+          type: Sequelize.DATEONLY,
+          allowNull: false,
+        },
+        planDoneCheck: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "Plan",
+        tableName: "Plan",
+        paranoid: false,
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.Plan.belongsTo(db.User);
+  }
 };
