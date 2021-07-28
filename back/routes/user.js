23f28564c43315358a4f7c61f16b6b9458b4ce7d
JhsK <qwe6293@nate.com>
2021-07-28 15:21:27 +0900

    [Feat] 카카오 로그인 응답 API 작성

--- back/routes/user.js
+++ back/routes/user.js
@@ -126,4 +126,16 @@ router.patch("/email", isLoggedIn, async (req, res, next) => {
   }
 });
 
+router.get("/kakao", passport.authenticate("kakao"));
+
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+
 module.exports = router;
